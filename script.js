9f598c3752e1e60e6d2b64066f315e69148362be
Alesia Lebedevich <lebedevi4@mail.ru>
2020-03-28 22:55:35 +0300

    feat: add responsive-task

--- script.js
+++ script.js
@@ -123,3 +123,23 @@ document.querySelector(".control.right").addEventListener("click", function() {
         nextItem(currentItem);
     }
 });
+
+// hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.hamburger-menu'),
+    menuItem = document.querySelectorAll('.menu_link'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
